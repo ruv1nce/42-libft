fe9bc497c125a6004fda012fc6f56d2b58587042
Dmitry Fonarev <dfonarev@e1z2r4p11.42.us.org>
2019-02-22 20:48:16 -0800

    edit

---
+++ Makefile
@@ -0,0 +1,28 @@
+NAME = libft.a
+
+SRCS = ft_bzero.c \
+	   ft_lstnew.c \
+	   ft_memccpy.c \
+	   ft_memchr.c \
+	   ft_memcmp.c \
+	   ft_memcpy.c \
+	   ft_memmove.c \
+	   ft_memset.c \
+	   ft_strlen.c
+
+OBJS = $(SRCS:%.c=%.o)
+
+all: $(NAME)
+
+$(NAME):
+	gcc -Wall -Wextra -Werror $(SRCS) -c $(OBJS)
+	ar rc $(NAME) $(OBJS)
+	ranlib $(NAME)
+
+clean:
+	rm -f $(OBJS)
+
+fclean:	clean
+	rm -f $(NAME)
+
+re: fclean all
---
+++ libft.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfonarev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/11 19:29:58 by dfonarev          #+#    #+#             */
+/*   Updated: 2019/02/22 19:23:02 by dfonarev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+void				*ft_memset(void *b, int c, size_t len);
+void				ft_bzero(void *s, size_t n);
+void				*ft_memcpy(void *dst, const void *src, size_t n);
+void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void				*ft_memmove(void *dst, const void *src, size_t len);
+void				*ft_memchr(const void *s, int c, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t				ft_strlen(const char *s);
+//t_list			*ft_lstnew(void const *content, size_t content_size);
+
+#endif
