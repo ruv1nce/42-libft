b550c92c855822c179b214148176cbadc3d09f06
Dmitry Fonarev <dfonarev@e1z2r4p11.42.us.org>
2019-02-23 02:09:57 -0800

    edit

---
+++ ft_strnstr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfonarev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/23 01:51:07 by dfonarev          #+#    #+#             */
+/*   Updated: 2019/02/23 02:09:08 by dfonarev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	char	*ptr;
+	char	*needorig;
+	size_t	tmp;
+
+	ptr = NULL;
+	needorig = (char *)needle;
+	tmp = 0;
+	if (!*needle)
+		return ((char *)haystack);
+	while (*haystack && len)
+	{
+		if (*haystack == *needle)
+		{
+			ptr = (char *)haystack;
+			while (*haystack == *needle && len)
+			{
+
+				haystack++;
+				needle++;
+				len--;
+				tmp++;
+			}
+			if (!*needle)
+				return (ptr);
+			if (!len)
+				return (NULL);
+			haystack = ptr;
+			needle = needorig;
+			len += tmp;
+			tmp = 0;
+		}
+		haystack++;
+		len--;
+	}
+	return (ptr);
+}
---
+++ ft_strstr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfonarev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/23 01:51:07 by dfonarev          #+#    #+#             */
+/*   Updated: 2019/02/23 01:54:02 by dfonarev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	char	*ptr;
+	char	*needorig;
+
+	ptr = NULL;
+	needorig = (char *)needle;
+	if (!*needle)
+		return ((char *)haystack);
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+		{
+			ptr = (char *)haystack;
+			while (*haystack == *needle)
+			{
+
+				haystack++;
+				needle++;
+			}
+			if (!*needle)
+				return (ptr);
+				haystack = ptr;
+				needle = needorig;
+		}
+		haystack++;
+	}
+	return (ptr);
+}
