e46134b342d8ff6a7735a873539331f7c3b342d2
Dmitry Fonarev <dfonarev@e1z2r4p11.42.us.org>
2019-02-25 06:46:38 -0800

    edit

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dfonarev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 02:23:21 by dfonarev          #+#    #+#             */
-/*   Updated: 2019/02/25 06:33:16 by dfonarev         ###   ########.fr       */
+/*   Updated: 2019/02/25 06:43:54 by dfonarev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*str1;
 	unsigned char	*str2;
 
+	if (n == 0)
+		return (0);
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	while (*str1 && *str2 && *str1 == *str2 && --n)
